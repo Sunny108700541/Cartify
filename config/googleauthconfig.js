da276d554c5502b2dce38a562d062f9629fa4af9
Sunny108700541 <sunnysinghchauhan082@gmail.com>
2025-03-19 22:47:23 +0530

    Update googleauthconfig.js

--- config/googleauthconfig.js
+++ config/googleauthconfig.js
@@ -5,7 +5,7 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.NODE_ENV === "production" ? "https://cartify-vawj.onrender.com/auth/google/callback" : "http://localhost:3000/auth/google/callback"
   },
   async function(accessToken, refreshToken, profile, cb) {
     try {
